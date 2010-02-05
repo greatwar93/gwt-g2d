0db213e8a792220e7a17eff9caf93fae692e34c4
hao1300 <hao1300@0cb6d47c-79f1-11de-be4f-dfe7ea969a89>
2010-02-05 14:05:35 +0000

    Updated shape builder to allow for non-connecting arcs.

--- gwt-g2d/src/gwt/g2d/client/graphics/visitor/ArcToVisitor.java
+++ gwt-g2d/src/gwt/g2d/client/graphics/visitor/ArcToVisitor.java
@@ -26,7 +26,43 @@ import gwt.g2d.client.math.Vector2;
  * @author hao1300@gmail.com
  */
 public class ArcToVisitor implements ShapeVisitor {
-	private final double x1, y1, x2, y2, radius;
+	private final double x0, y0, x1, y1, x2, y2, radius;
+	private final boolean connectFromPrev;
+	
+	/**
+	 * <p>
+	 * If the point (x0, y0) is equal to the point (x1, y1), or if the point 
+	 * (x1, y1) is equal to the point (x2, y2), or if the radius radius is zero, 
+	 * then the method must add the point (x1, y1) to the subpath, and connect 
+	 * that point to the previous point (x0, y0) by a straight line.
+	 * <p>
+	 * Otherwise, if the points (x0, y0), (x1, y1), and (x2, y2) all lie on a 
+	 * single straight line, then the method must add the point (x1, y1) to the 
+	 * subpath, and connect that point to the previous point (x0, y0) by a 
+	 * straight line.
+	 * <p>
+	 * Otherwise, let The Arc be the shortest arc given by circumference of the 
+	 * circle that has radius radius, and that has one point tangent to the 
+	 * half-infinite line that crosses the point (x0, y0) and ends at the point 
+	 * (x1, y1), and that has a different point tangent to the half-infinite line 
+	 * that ends at the point (x1, y1) and crosses the point (x2, y2). The points 
+	 * at which this circle touches these two lines are called the start and end 
+	 * tangent points respectively. The method must connect the point (x0, y0) to 
+	 * the start tangent point by a straight line, adding the start tangent point 
+	 * to the subpath, and then must connect the start tangent point to the end 
+	 * tangent point by The Arc, adding the end tangent point to the subpath.
+	 */
+	public ArcToVisitor(double x0, double y0, double x1, double y1, 
+			double x2, double y2, double radius) {
+		this.x0 = x0;
+		this.y0 = y0;
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.radius = radius;
+		this.connectFromPrev = false;
+	}
 	
 	/**
 	 * Let the point (x0, y0) be the last point in the subpath.
@@ -53,11 +89,23 @@ public class ArcToVisitor implements ShapeVisitor {
 	 * tangent point by The Arc, adding the end tangent point to the subpath.
 	 */
 	public ArcToVisitor(double x1, double y1, double x2, double y2, double radius) {
+		this.x0 = 0;
+		this.y0 = 0;
 		this.x1 = x1;
 		this.y1 = y1;
 		this.x2 = x2;
 		this.y2 = y2;
 		this.radius = radius;
+		this.connectFromPrev = true;
+	}
+	
+	/**
+	 * @see #ArcToVisitor(double, double, double, double, double, double, double)
+	 */
+	public ArcToVisitor(Vector2 point0, Vector2 point1, Vector2 point2, 
+			double radius) {
+		this(point0.getX(), point0.getY(), point1.getX(), point1.getY(), 
+				point2.getX(), point2.getY(), radius);
 	}
 	
 	/**
@@ -69,6 +117,9 @@ public class ArcToVisitor implements ShapeVisitor {
 	
 	@Override
 	public void visit(Surface surface) {
+		if (!connectFromPrev) {
+			surface.getContext().moveTo(x0, y0);
+		}
 		surface.getContext().arcTo(x1, y1, x2, y2, radius);
 	}
 }
