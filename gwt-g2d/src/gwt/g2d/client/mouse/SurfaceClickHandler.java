b87dc4cc6a6048ce9daf6fe6e94d99c14b5e3bac
Karel.Crombecq <Karel.Crombecq@gmail.com>
2012-02-29 14:00:47 +0000

    updated click handling

--- gwt-g2d/src/gwt/g2d/client/mouse/SurfaceClickHandler.java
+++ gwt-g2d/src/gwt/g2d/client/mouse/SurfaceClickHandler.java
@@ -3,5 +3,5 @@ package gwt.g2d.client.mouse;
 import gwt.g2d.shared.math.Vector2;
 
 public interface SurfaceClickHandler {
-	public void onClick(Vector2 pos);
+	public void onClick(Vector2 pos, Long id);
 }
